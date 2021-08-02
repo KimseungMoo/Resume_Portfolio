2f9f1fbdebad4d2d33cef2ab64158d1c691fefa5
Kim SeungMoo <tmdan1346@gmail.com>
2021-08-02 15:53:47 +0900

    feat/project

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
 import About from "./pages/About";
+import Project from "./pages/Project";
 
 function App() {
   return (
     <>
       <LandingPage />
       <About />
+      <Project />
     </>
   );
 }
